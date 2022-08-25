5e718ecea6fe4b5153c9146d7c8e3174e375130f
Shekhar Verma <shekhar17verma@gmail.com>
2022-08-25 23:49:02 +0530

    25aug changes

---
+++ leetcode-problem-solutions/sliding-window/longest-substring-without-repeating-characters.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLongestSubstring = function(s) {
+    if(!s.length) return 0;
+    let strSet = new Set(), longesSub = 1;
+    for(i=0; i<s.length; i++){
+        if(strSet.has(s[i])){
+            longesSub = Math.max(longesSub, strSet.size)
+            strSet.clear()
+        }
+        strSet.add(s[i])
+    }
+    longesSub = Math.max(longesSub, strSet.size)
+    return longesSub;
+};
