149f43eb3f1b60b077c8f1492571b93e7b46d218
Shekhar Verma <shekhar17verma@gmail.com>
2022-09-01 22:44:09 +0530

    01sept changes

---
+++ leetcode-problem-solutions/DP/climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function(n) {
+    let one = two = 1
+    for(let i=1;i<n;i++){
+        let temp = one
+        one = one+two
+        two= temp
+    }
+    return one;
+};
