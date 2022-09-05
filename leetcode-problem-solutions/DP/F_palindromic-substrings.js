b422b2505007e239cc11a76e0a0216af39309ed1
Shekhar Verma <shekhar17verma@gmail.com>
2022-09-05 22:15:19 +0530

    03sept changes

---
+++ leetcode-problem-solutions/DP/F_palindromic-substrings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ * 
+ * https://leetcode.com/problems/palindromic-substrings/submissions/
+ * 
+ */
+ var countSubstrings = function(s) {
+    let res = 0
+    for(i = 0; i<s.length;i++){
+       countPali(i, i)
+       countPali(i, i+1)
+    }
+   function countPali(l, r ){
+       while(l>-1 && r<s.length && s[l] === s[r]){
+            res +=1
+            l -= 1
+            r += 1
+        }
+   }
+       return res
+   };
